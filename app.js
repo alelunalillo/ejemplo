20be2e73d75bd2a3ec1b425925e8677cb466bf52
alelunalillo <aluna@itpfp.com>
2023-10-05 09:45:26 +0200

    primer commit

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-//app.get('/', function (req, res){
-   // res.sendFile(path.resolve(__dirname, ''))
-//})
+app.get('/', function (req, res){
+   res.sendFile(path.resolve(__dirname, 'Inicio.html'))
+})
 
 app.get('/', function (req, res){
 res.send('Hola mundo')
